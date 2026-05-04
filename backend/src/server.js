7afb85eaf035792a49f37f8c17d3a90d5073c522
cksrana <cks.rana@gmail.com>
2026-05-04 15:20:03 +0600

    ready for deploymenty

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,31 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
+// import Path2D from "path";
+ 
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
+const __dirname = path.resolve();
+//const __dirname = Path2D.resolve(); 
 
+process.env.PORT = process.env.PORT || 5000;
+
+
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-process.env.PORT = process.env.PORT || 5000;
+//make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
